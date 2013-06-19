fc5d51314b12792fdd2c6677020122a18df32b45
Maksim Sokolski <misokolsky@gmail.com>
2013-06-19 13:20:04 +0300

    Refactor.

--- life.py
+++ life.py
@@ -11,14 +11,14 @@ def count(matrix):
 
     for i in range(1, cells+1):
         for j in range(1, cells+1):
-            result[i-1, j-1] += extended[i-1,j-1] + extended[i,j-1] +\
-                                extended[i+1,j-1] + extended[i+1,j] +\
-                                extended[i+1,j+1] + extended[i,j+1] +\
-                                extended[i-1,j+1] + extended[i-1,j]
+            result[i-1, j-1] += (extended[i-1,j-1] + extended[i,j-1] +
+                                 extended[i+1,j-1] + extended[i+1,j] +
+                                 extended[i+1,j+1] + extended[i,j+1] +
+                                 extended[i-1,j+1] + extended[i-1,j])
 
     return result
 
-        
+
 def show(world):
     color = {0: (0,0,0), 1: (0,0,255)}
 
@@ -26,7 +26,7 @@ def show(world):
         for j in range(cells):
             pygame.draw.circle(screen, color[world[i][j]],
                                (20+i+25*i, 20+j+25*j), 5)
-            
+   
 
 def nextgen():
     next = count(numpy.array(world))
@@ -45,13 +45,13 @@ def nextgen():
 if __name__ == '__main__':
     cells = 25
     tick = 200
-    
+
     world = [[0]*cells for i in range(cells)]
     world = [[random.randrange(0,2) for j in i] for i in world]
-    
+
     screen = pygame.display.set_mode((cells*27, cells*27))
     pygame.display.set_caption("Conway's game of life")
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
